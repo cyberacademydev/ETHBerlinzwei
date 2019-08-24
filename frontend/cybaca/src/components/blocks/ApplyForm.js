ecde6a7ad28ea02e03903c2191da814ffbd75e64
zhukreal <zhukreal@gmail.com>
2019-08-24 21:54:20 +0200

    test

--- frontend/cybaca/src/components/blocks/ApplyForm.js
+++ frontend/cybaca/src/components/blocks/ApplyForm.js
@@ -9,16 +9,17 @@ export class ApplyForm extends Component {
         name: '',
         bio: '',
         topic: '',
-        duration: '',
+        duration: 15,
         proof: ''
-      }
+      },
+        loading: false
     };
     // this.smart = '0x61B81103e716B611Fff8aF5A5Dc8f37C628efb1E';
     this.smart = ADRESS;
   }
 
   componentDidMount() {
-
+      this.getMe()
   }
 
   onChange = e => {
@@ -31,6 +32,61 @@ export class ApplyForm extends Component {
     e.preventDefault();
     this.getAccount();
   };
+
+
+  getMe = async () => {
+    const { web3, setWarning } = this.props;
+    if (web3.currentProvider.host)
+      return setWarning(
+          'Non-Ethereum browser detected. You should consider trying MetaMask!'
+      );
+    if (window.ethereum) {
+      try {
+        const accounts = await window.ethereum.enable();
+        if (accounts.length) {
+          this.setMe(accounts[0])
+        }
+      } catch (error) {
+        clearInterval(this.interval);
+        setWarning('You declined transaction', error);
+      }
+    } else if (window.web3) {
+      const accounts = await web3.eth.getAccounts();
+      if (accounts.length) {
+        this.setMe(accounts[0])
+      }
+    } else return setWarning('Your metamask is locked!');
+  }
+
+  setMe = (account) => {
+    const { web3, contract, deposit } = this.props;
+    try {
+
+
+      // web3.eth
+      //     .sendTransaction({
+      //       from: account,
+      //       to: this.smart,
+      //       data: contract.methods
+      //           .getTalkById(
+      //               1
+      //           )
+      //           .encodeABI()
+      //     })
+      //     .on('transactionHash', h => {
+      //
+      //     })
+      //     .on('confirmation', (confirmationNumber, receipt) => {
+      //       debugger
+      //     })
+      //     .on('error', err => {
+      //       console.log('error', err);
+      //     });
+    } catch (e) {
+      console.log(e);
+      this.setState({loading: false})
+    }
+  }
 
   getAccount = async () => {
     console.log('get account')
@@ -59,7 +115,9 @@ export class ApplyForm extends Component {
 
   applyForm = account => {
     const { web3, contract, deposit } = this.props;
-    const { data } = this.state;
+    let data = Object.assign({}, this.state.data);
+    data.duration = data.duration * 60
+    this.setState({loading: true})
     if (deposit) {
       const depositInWei = web3.utils.toWei(deposit, 'ether');
       try {
@@ -82,13 +140,16 @@ export class ApplyForm extends Component {
             console.log('transactionHash', h);
           })
           .on('confirmation', (confirmationNumber, receipt) => {
+            debugger
             console.log('confirmation', confirmationNumber, receipt);
+              this.setState({loading: true, sent: true})
           })
           .on('error', err => {
             console.log('error', err);
           });
       } catch (e) {
         console.log(e);
+          this.setState({loading: false})
       }
     }
   };
@@ -164,7 +225,7 @@ export class ApplyForm extends Component {
             {/*  onChange={this.onChange}*/}
             {/*/>*/}
             <select className={'s-duration'} name="duration" id="duration" value={data.duration} onChange={this.onChange}>
-              {[5, 10, 15, 20, 25, 30, 35, 40, 45,50, 55, 60].map((item, key) => {
+              {[15, 20, 25, 30, 35, 40, 45,50, 55, 60].map((item, key) => {
                 return <option key={key} value={item}>{item}</option>
               })}
             </select>
@@ -202,9 +263,9 @@ export class ApplyForm extends Component {
           <button
             className="block-button-white"
             style={{ margin: '35px 0 0 45px' }}
-            disabled={!Object.keys(this.props.deposit).length}
+            disabled={!Object.keys(this.props.deposit).length || this.state.loading}
           >
-            APPLY
+              {this.state.sent? 'APPROVING...' : 'APPLY' }
           </button>
         </form>
       </Fragment>
