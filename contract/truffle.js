c4853176d9238dcd240b0128b1cf1a32151f5306
Arsenii Pechenkin <arspechen@gmail.com>
2019-08-25 09:36:30 +0300

    add skale

--- contract/truffle.js
+++ contract/truffle.js
@@ -4,6 +4,10 @@ const HDWalletProvider = require('truffle-hdwallet-provider');
 const infuraConfigRinkeby = require('./infura_rinkeby_deploy.json');
 const infuraConfigMainnet = require('./infura_main_deploy.json');
 
+const skalePrivateKey = "0xa06bbd0e41f35e0f5c9251746a41b1cb47f75ec7d15630edfa313198c389953c"
+const skaleUrl = "https://ethberlin01.skalenodes.com:10216"
+const skalePublicKey = "0x01206c58b70068Bb86792ac91c0E6F3aE96cCA77"
+
 module.exports = {
     
     migrations_directory: "./migrations",
@@ -33,7 +37,17 @@ module.exports = {
             host: "localhost",
             port: 7545,
             network_id: "5777"
-        }
+        },
+
+        skale: {
+          provider() {
+            return new HDWalletProvider(skalePrivateKey, skaleUrl);
+          },
+          from: skalePublicKey,
+          network_id: 0,
+          gasPrice: toWei(50, 'gwei').toNumber(),
+          gas: toWei(6, 'mwei').toNumber()
+        },
     },
     
     compilers: {
